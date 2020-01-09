530b52c2e85b6b34b3cd7f10f637b7b00c22f176
ShaharGotshtat <shahar.g@logz.io>
2020-01-09 19:44:00 +0200

    Add post endpoint

--- app/app.py
+++ app/app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask import abort
+from flask import request
+from flask import jsonify
+
+from db_connector import execute_create_or_update_query, execute_get_query
 
 app = Flask(__name__)
+
+
+@app.route('/mini-hacker-news/api/v1/post', methods=['POST'])
+def add_post():
+    if not (request.json and 'text' in request.json):
+        app.logger.info("Invalid request params")
+        abort(400)
+
+    text = request.json["text"]
+    results = execute_create_or_update_query(f'INSERT INTO `post`(text) VALUES ("{text}");')
+    return jsonify(results)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
