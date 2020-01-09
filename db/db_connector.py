da00cdc243ccdf9d39111dcd652dd833a5807a8c
ShaharGotshtat <shahar.g@logz.io>
2020-01-09 19:35:44 +0200

    Add functions to the db_connector

--- db/db_connector.py
+++ db/db_connector.py
@@ -10,3 +10,25 @@ def get_db_connection():
         'database': 'mini-hacker-news'
     }
     return mysql.connector.connect(**config)
+
+
+def execute_create_or_update_query(query):
+    connection = get_db_connection()
+    cursor = connection.cursor(buffered=True)
+    cursor.execute(query)
+    connection.commit()
+    results = cursor.lastrowid
+    cursor.close()
+    connection.close()
+    return results
+
+
+def execute_get_query(query):
+    connection = get_db_connection()
+    cursor = connection.cursor(buffered=True)
+    cursor.execute(query)
+    connection.commit()
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return results
